3d551e99a2931108c7b6c61b15b9a0333435c592
unknown <you@example.com>
2022-12-29 01:12:52 +0300

    task

---
+++ 28-task/28-task/28-task.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="28-task.aspx.cs" Inherits="_28_task._28_task" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+  
+   
+</head>
+<body>
+    <form id="form1" runat="server">
+     
+        <asp:ScriptManager ID="ScriptManager1" runat="server">
+        </asp:ScriptManager>
+        <asp:UpdatePanel ID="UpdatePanel1" runat="server">
+            <ContentTemplate>
+                <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+                <asp:Button ID="Button1" runat="server" Text="Button" OnClick="Button1_Click" />
+        <br />
+     
+            </ContentTemplate>
+        </asp:UpdatePanel>
+    </form>
+</body>
+</html>
---
+++ 28-task/28-task/28-task.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace _28_task
+{
+    public partial class _28_task : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+            SqlConnection connection =
+            new SqlConnection("data source =  DESKTOP-EK069V4\\SQLEXPRESS; database = lubna ; integrated security=SSPI");
+            SqlCommand command = new SqlCommand("Insert into task28(comment) values(@name)", connection);
+            connection.Open();
+            command.Parameters.AddWithValue("@name", TextBox1.Text);
+            command.ExecuteNonQuery();
+            connection.Close();
+
+            //    TextBox1.Text = "";
+            //    ScriptManager.RegisterStartupScript(this, GetType(), "Popup", "successalert();", true);
+            //}
+
+            //protected void Timer1_Tick(object sender, EventArgs e)
+            //{
+
+            //}
+        }
+    }
+}
